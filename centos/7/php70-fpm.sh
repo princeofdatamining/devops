0bcb3d658f495f5cf45c24599060c4420a49c83c
princeofdatamining <princeofdatamining@gmail.com>
2018-03-05 23:21:24 +0800

    Create php70-fpm.sh

---
+++ centos/7/php70-fpm.sh
@@ -0,0 +1,20 @@
+cat <<EOF > /usr/lib/systemd/system/php-fpm.service
+[Unit]
+Description=The PHP FastCGI Process Manager
+After=syslog.target network.target
+
+[Service]
+Type=notify
+PIDFile=/var/opt/remi/php70/run/php-fpm/php-fpm.pid
+EnvironmentFile=/etc/opt/remi/php70/sysconfig/php-fpm
+ExecStart=/opt/remi/php70/root/usr/sbin/php-fpm --nodaemonize
+ExecReload=/bin/kill -USR2 \$MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable php-fpm
+systemctl restart php-fpm
+systemctl status php-fpm
