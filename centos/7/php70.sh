406bdfb067c2af1d1d41687f1a203ec90a4b667e
princeofdatamining <princeofdatamining@gmail.com>
2018-03-05 23:07:07 +0800

    Create php70.sh

---
+++ centos/7/php70.sh
@@ -0,0 +1,25 @@
+yum install -y \
+  php70 \
+  php70-php-fpm \
+  php70-php-gd \
+  php70-php-ldap \
+  php70-php-exif \
+  php70-php-intl \
+  php70-php-mbstring \
+  php70-php-mcrypt \
+  php70-php-mysqli \
+  php70-php-opcache \
+  php70-php-pdo_mysql \
+  php70-php-pgsql \
+  php70-php-pdo_pgsql \
+  php70-php-zip \
+  php70-php-pcntl \
+  php70-php-xml \
+  php70-php-posix \
+  php70-php-smbclient \
+  php70-php-imap \
+  php70-php-gmp \
+  php70-php-apcu \
+  php70-php-memcached \
+  php70-php-redis \
+  php70-php-imagick
