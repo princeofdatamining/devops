7b265123666f4f3eba90f95a54a97d235adc9598
princeofdatamining <princeofdatamining@gmail.com>
2018-04-25 22:15:08 +0800

    Update get-assign-ip.sh

--- DigitalOcean/get-assign-ip.sh
+++ DigitalOcean/get-assign-ip.sh
@@ -1,2 +1,6 @@
 curl -L -o /usr/local/bin/assign-ip http://do.co/assign-ip
 chmod +x /usr/local/bin/assign-ip
+
+mkdir /usr/lib/ocf/resource.d/digitalocean
+curl -L -o /usr/lib/ocf/resource.d/digitalocean/floatip http://do.co/ocf-floatip
+chmod +x /usr/lib/ocf/resource.d/digitalocean/floatip
