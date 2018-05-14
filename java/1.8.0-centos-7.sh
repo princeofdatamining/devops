7f31438af1975c1b7390f03f68eb9d2a0c693fe6
princeofdatamining <princeofdatamining@gmail.com>
2018-05-14 15:51:23 +0800

    Update 1.8.0-centos-7.sh

--- java/1.8.0-centos-7.sh
+++ java/1.8.0-centos-7.sh
@@ -1,3 +1,9 @@
 yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel
 
-echo "JAVA_HOME=/usr/lib/jvm/java" >> ~/.bash_profile
+# The JAVA_HOME variable should be set to a directory which contains the executable file bin/java.
+#echo "export JAVA_HOME=/usr/lib/jvm/java" >> ~/.bash_profile
+
+# Most modern Linux operating systems provide a mechanism, such as /usr/bin/alternatives on RHEL or CentOS, 
+# for transparently switching between versions of executables such as Java. 
+# In this case, you can set JAVA_HOME to the directory containing the symbolic link to bin/java, which is usually /usr.
+echo "export JAVA_HOME=/usr" >> ~/.bash_profile
