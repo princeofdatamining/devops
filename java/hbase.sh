d16b62ca9045cb576d4010aad9113d2ccb156462
princeofdatamining <princeofdatamining@gmail.com>
2018-05-14 16:26:16 +0800

    Update hbase.sh

--- java/hbase.sh
+++ java/hbase.sh
@@ -2,5 +2,5 @@ HBASE_VER=2.0.0
 
 wget http://www-us.apache.org/dist/hbase/$HBASE_VER/hbase-$HBASE_VER-bin.tar.gz
 tar -zxf hbase-$HBASE_VER-bin.tar.gz -C /usr/share/java/
+rm -rf hbase-$HBASE_VER-bin.tar.gz
 ln -s /usr/share/java/hbase-$HBASE_VER/bin/hbase /usr/local/bin/
-rm -f hbase-$HBASE_VER-bin.tar.gz
