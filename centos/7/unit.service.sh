813244ca7250c7d4ee12ac4abd95537dff7ef98f
princeofdatamining <princeofdatamining@gmail.com>
2018-05-14 22:59:45 +0800

    Create unit.service.sh

---
+++ centos/7/unit.service.sh
@@ -0,0 +1,10 @@
+cat <<EOF > /etc/yum.repos.d/unit.repo
+[unit]
+name=unit repo
+baseurl=https://packages.nginx.org/unit/centos/\$releasever/\$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum install -y unit unit-python unit-go unit-php
+systemctl enable unit
