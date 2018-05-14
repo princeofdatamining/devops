40dcdee151c972427c51f509c7976aff9ab8356c
princeofdatamining <princeofdatamining@gmail.com>
2018-05-14 16:25:50 +0800

    Create hadoop.sh

---
+++ java/hadoop.sh
@@ -0,0 +1,9 @@
+HADOOP_VER=3.1.0
+
+wget http://www-us.apache.org/dist/hadoop/common/hadoop-$HADOOP_VER/hadoop-$HADOOP_VER.tar.gz
+tar -zxf hadoop-$HADOOP_VER.tar.gz -C /usr/share/java/
+rm -rf hadoop-$HADOOP_VER.tar.gz
+#ln -s /usr/share/java/hadoop-$HADOOP_VER/bin/hadoop /usr/local/bin/
+#ln -s /usr/share/java/hadoop-$HADOOP_VER/bin/hdfs /usr/local/bin/
+#ln -s /usr/share/java/hadoop-$HADOOP_VER/bin/mapred /usr/local/bin/
+#ln -s /usr/share/java/hadoop-$HADOOP_VER/bin/yarn /usr/local/bin/
