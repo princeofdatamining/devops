7c6b0f7c4d49d38d7b448837b8bc943b7018892a
princeofdatamining <princeofdatamining@gmail.com>
2018-05-14 15:51:49 +0800

    Update tomcat.sh

--- java/tomcat.sh
+++ java/tomcat.sh
@@ -2,5 +2,5 @@ MAJOR_VERSION=9
 VERSION=9.0.7
 wget https://mirrors.aliyun.com/apache/tomcat/tomcat-$MAJOR_VERSION/v$VERSION/bin/apache-tomcat-$VERSION.tar.gz
 tar -xzf apache-tomcat-$VERSION.tar.gz -C /usr/share/java/
-rm apache-tomcat-$VERSION.tar.gz
+rm -f apache-tomcat-$VERSION.tar.gz
 /usr/share/java/apache-tomcat-$VERSION/bin/startup.sh
