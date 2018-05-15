5258b11330c64e0beb123c1b66139f7269602aab
princeofdatamining <princeofdatamining@gmail.com>
2018-05-15 11:31:25 +0800

    Create pyenv.sh

---
+++ python/pyenv.sh
@@ -0,0 +1,17 @@
+if [ x"" == x"$1" ]; then
+  PYENV="$(realpath ~/.pyenv)"
+else
+  PYENV="$(realpath "$1")"
+fi
+
+cat <<EOF >> ~/.bash_profile
+export PYENV_ROOT="$PYENV"
+export PATH="\$PYENV_ROOT/bin:\$PATH"
+eval "\$(pyenv init -)"
+eval "\$(pyenv virtualenv-init -)"
+EOF
+
+export PYENV_ROOT="$PYENV"
+
+# https://github.com/pyenv/pyenv-installer
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
