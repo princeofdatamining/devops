97f4aac8aa5eefbc84a4501f8bd5d599e36b9b20
princeofdatamining <princeofdatamining@gmail.com>
2018-05-15 15:41:40 +0800

    Update pyenv.sh

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -15,3 +15,7 @@ export PYENV_ROOT="$PYENV"
 
 # https://github.com/pyenv/pyenv-installer
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
+yum install -y \
+wget curl make gcc patch \
+openssl-devel zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel
