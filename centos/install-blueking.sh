402309b7169c3ca70a696781255ead8b6f62ddfc
princeofdatamining <princeofdatamining@gmail.com>
2018-09-27 13:33:39 +0800

    blueking installation

---
+++ centos/install-blueking.sh
@@ -0,0 +1,6 @@
+sudo yum install -y wget git curl
+
+wget http://bkopen-1252002024.file.myqcloud.com/ce4.1/install_ce-master-1.3.5.tgz
+wget http://bkopen-1252002024.file.myqcloud.com/ce4.1/bkce_src-4.1.15.tgz
+
+# http://bk.tencent.com/download/#ssl
