405c6cb84ad10b22baa79dbd42ca8e9dd5be7af1
JiPeng <jipeng@cnicg.cn>
2019-05-22 19:23:31 +0800

    fix: pyenv-install

--- generic/install-pyenv.sh
+++ generic/install-pyenv.sh
@@ -1,15 +1,23 @@
 # https://github.com/pyenv/pyenv-installer
-curl -fsSL https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | source
+curl -fsSL https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | $SHELL
+
+## Load pyenv automatically by adding
+## the following to ~/.bashrc:
+#
+# export PYENV_ROOT=/cnicg/app/pyenv
+# export PATH="${PYENV_ROOT}/bin:$PATH"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
 
 if [ "$(uname -s)" = "Darwin" ]; then
     echo "prepare brew and xcode for Mac OS X"
     brew install openssl readline sqlite3 xz zlib
     xcode-select --install
 elif which yum > /dev/null; then
-    yum install -y \
+    sudo yum install -y \
         git wget curl make gcc patch \
         openssl-devel readline-devel sqlite sqlite-devel \
-        zlib-devel bzip2 bzip2-devel
+        zlib-devel bzip2 bzip2-devel libffi-devel
 elif which apt-get > /dev/null; then
     apt-get install -y \
         git wget curl make gcc \
@@ -17,3 +25,5 @@ elif which apt-get > /dev/null; then
         zlib1g-dev libbz2-dev \
         build-essential llvm libncurses5-dev xz-utils
 fi
+
+[ -d ~/.pyenv ] && mv ~/.pyenv /cnicg/app/pyenv
